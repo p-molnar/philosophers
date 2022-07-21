13f216a43ccb16fe0e7b0230eb364ff7ecb5757f
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-22 01:28:52 +0200

    redesign data structure

--- philo/include/philo_data_structures.h
+++ philo/include/philo_data_structures.h
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/07 12:19:42 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/06/30 13:44:12 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/07/22 00:35:15 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,43 +22,14 @@
 typedef struct timeval	t_time;
 typedef pthread_mutex_t	t_mutex;
 
-typedef struct s_log
-{
-	int		log_status;
-	t_time	log_time;
-	int		philo_status;
-	size_t	philo_id;
-}	t_log;
-
-typedef struct s_sim_data
-{
-	int			n_philo;
-	int			t_die;
-	int			t_eat;
-	int			t_sleep;
-	int			min_n_eat;
-	bool		all_philo_alive;
-	pthread_t	checker;
-	pthread_t	printer;
-	t_log		*log_queue;
-	t_time		start_time;
-	t_mutex		queue_lock;
-	t_mutex		monitoring_lock;
-	t_mutex		start_lock;
-	t_mutex		**forks;
-}	t_sim_data;
-
 typedef struct s_philo
 {
-	size_t		id;
-	int			status;
-	int			n_eat;	
-	t_time		last_time_eaten;
-	t_time		last_action_time;
-	t_mutex		*left_fork;
-	t_mutex		*right_fork;
-	pthread_t	thread;
-	t_sim_data		*sim_attr;
 }	t_philo;
+
+typedef struct s_data
+{
+	int		arg[5];
+	t_philo	*philo;
+}	t_data;
 
 #endif
