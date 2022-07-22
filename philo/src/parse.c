8584243611733f36f7efc1301e8bb7b0437f92a0
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-22 23:51:51 +0200

    change error handling, and data types

--- philo/src/parse.c
+++ philo/src/parse.c
@@ -6,39 +6,39 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/07 12:54:06 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/07/22 17:00:38 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/07/22 22:39:28 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
-int	validate_arg(int argc, t_data *data)
+int	validate_arg(int argc, t_sim *data)
 {
 	uint16_t	i;
 
-	if (data->arg[N_PHILO] < 1)
-		return (error_handler(INVALID_INPUT, "validate_arg"));
+	if (data->attr[N_PHILO] < 1)
+		return (error_handler(INPUT_ERR_MSG, __FILE__,__LINE__));
 	i = 0;
 	while (i < argc - 1)
 	{
-		if (data->arg[i] < 0)
-			return (error_handler(INVALID_INPUT, "validate_arg"));
+		if (data->attr[i] < 0)
+			return (error_handler(INPUT_ERR_MSG, __FILE__,__LINE__));
 		i++;
 	}
 	return (EXIT_SUCCESS);
 }
 
-int	parse_args(int argc, char *argv[], t_data *data)
+int	parse_args(int argc, char *argv[], t_sim *data)
 {	
 	uint16_t	i;
 
 	if (argc < 5 || argc > 6)
-		return (error_handler(INVALID_INPUT, "parse_args"));
+		return (error_handler(INPUT_ERR_MSG, __FILE__,__LINE__));
 	i = 1;
 	while (i < argc)
 	{
-		data->arg[i - 1] = ft_atoi(argv[i]);
-		data->arg[i] = 0;
+		data->attr[i - 1] = ft_atoi(argv[i]);
+		data->attr[i] = 0;
 		i++;
 	}
 	return (validate_arg(argc, data));
