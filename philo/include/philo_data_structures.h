cd64503b3021d1d2e3427af6b2c59009231af749
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-22 23:43:28 +0200

    create basic data structure for the project - to be extended

--- philo/include/philo_data_structures.h
+++ philo/include/philo_data_structures.h
@@ -6,30 +6,34 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/07 12:19:42 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/07/22 00:35:15 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/07/22 23:21:47 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_DATA_STRUCTURES_H
 # define PHILO_DATA_STRUCTURES_H
 
-# include <stddef.h>
-# include <philo_consts.h>
-# include <sys/time.h>
-# include <pthread.h>
-# include <stdbool.h>
+# define MUTEX_SIZE 1
 
 typedef struct timeval	t_time;
 typedef pthread_mutex_t	t_mutex;
 
 typedef struct s_philo
 {
+	pthread_t	thread;
 }	t_philo;
 
-typedef struct s_data
+typedef struct s_sim
 {
-	int		arg[5];
+	int		attr[5];
+	t_mutex	mutex[MUTEX_SIZE];
+	t_mutex	*fork;
 	t_philo	*philo;
-}	t_data;
+}	t_sim;
 
+typedef struct s_thread_data
+{
+	t_philo	*philo;
+	t_sim	*data;
+}	t_thread_data;
 #endif
