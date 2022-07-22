73785714c88093ec603b917e2556327062ad62bf
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-22 17:38:20 +0200

    add validation for required philo count

--- philo/src/parse.c
+++ philo/src/parse.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/07 12:54:06 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/07/22 01:21:18 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/07/22 17:00:38 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	validate_arg(int argc, t_data *data)
 {
 	uint16_t	i;
 
+	if (data->arg[N_PHILO] < 1)
+		return (error_handler(INVALID_INPUT, "validate_arg"));
 	i = 0;
 	while (i < argc - 1)
 	{
