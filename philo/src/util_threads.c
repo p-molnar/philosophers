a28a675503085f644fd94265d5bceef905ade126
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-25 14:09:46 +0200

    initial commit

---
+++ philo/src/util_threads.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   util_threads.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/07/25 14:06:36 by pmolnar       #+#    #+#                 */
+/*   Updated: 2022/07/25 14:08:00 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <philo.h>
+
+int	checker(t_sim *data)
+{
+	(void) data;
+}
+
+int	printer(t_sim *data)
+{
+	(void) data;
+}
