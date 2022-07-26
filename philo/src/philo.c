c21fc2369303ce02446e521b554a4462ab0411cb
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-27 00:16:02 +0200

    add philo routines and start on philo_think routine

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,37 +6,37 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/22 21:20:35 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/07/26 17:19:38 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/07/26 23:02:01 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
-void	p_think(t_philo *data)
+static void	philo_think(t_philo *data)
+{
+	log_status(data, THINKING, get_time());
+}
+
+void	philo_eat(t_philo *data)
 {
 	(void) data;
 }
 
-void	p_eat(t_philo *data)
+void	philo_sleep(t_philo *data)
 {
 	(void) data;
 }
 
-void	p_sleep(t_philo *data)
+uint16_t	simulation(t_philo *philo)
 {
-	(void) data;
-}
-
-int	simulation(t_philo *philo)
-{
-	pthread_mutex_lock(philo->m);
-	printf("philo id: %d\n", philo->id);
-	pthread_mutex_unlock(philo->m);
-	// while (true)
-	// {
-	// 	p_think(data);
-	// 	p_eat(data);
-	// 	p_sleep(data);
-	// }
+	pthread_mutex_lock(&philo->sim_data->mutex[INIT]);
+	// printf("philo id: %d\n", philo->id);
+	pthread_mutex_unlock(&philo->sim_data->mutex[INIT]);
+	while (true)
+	{
+		philo_think(philo);
+		philo_eat(philo);
+		philo_sleep(philo);
+	}
 	return (EXIT_SUCCESS);
 }
