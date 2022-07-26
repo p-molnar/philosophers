fa778dd6222a3b99d005ab194628582470d82fa9
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-27 00:15:20 +0200

    add queue printer func

--- philo/src/util_threads.c
+++ philo/src/util_threads.c
@@ -6,22 +6,33 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/25 14:06:36 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/07/25 14:30:15 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/07/26 22:59:41 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
-int	checker(t_sim *data)
+uint16_t	checker(t_sim *data)
 {
 	(void) data;
 	printf("checker started\n");
 	return (EXIT_SUCCESS);
 }
 
-int	printer(t_sim *data)
+uint16_t	printer(t_sim *data)
 {
-	(void) data;
-	printf("printer started\n");
+	uint16_t	i;
+
+	i = 0;
+	while (data->is_running)
+	{
+		if (data->queue[i].status != UNDEFINED)
+		{
+			print_status(data->queue[i]);
+			if (++i == QUEUE_SIZE)
+				i = 0;
+		}
+		usleep(500);
+	}
 	return (EXIT_SUCCESS);
 }
