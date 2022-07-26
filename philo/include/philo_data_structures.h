501fef374df8310bff4122df9f84d7fd244c154c
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-26 23:36:10 +0200

    add new entry to structs

--- philo/include/philo_data_structures.h
+++ philo/include/philo_data_structures.h
@@ -6,33 +6,48 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/07 12:19:42 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/07/26 17:38:47 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/07/26 22:45:10 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_DATA_STRUCTURES_H
 # define PHILO_DATA_STRUCTURES_H
 
-# define MUTEX_SIZE 1
+# define MUTEX_SIZE 3
+# define QUEUE_SIZE 1024
+
 # include <stdint.h>
 
 typedef struct timeval	t_time;
 typedef pthread_mutex_t	t_mutex;
+typedef struct s_sim	t_sim;
 
 typedef struct s_philo
 {
 	pthread_t	thread;
+	t_time		last_ate;
 	uint16_t	id;
-	t_mutex		*m;
+	t_sim		*sim_data;
 }	t_philo;
+
+typedef struct s_log
+{
+	int16_t		status;
+	uint16_t	timestamp;
+	uint16_t	philo_id;
+}	t_log;
 
 typedef struct s_sim
 {
 	int16_t		attr[5];
+
 	t_mutex		mutex[MUTEX_SIZE];
 	t_mutex		*fork;
 	t_philo		*philo;
 	pthread_t	thread[2];
+	t_log		queue[QUEUE_SIZE];
+	bool		is_running;
+	t_time		start_time;
 }	t_sim;
 
 #endif
