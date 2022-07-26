d4c1ac3cf0a3b589474b78db60b5afb74d47af1c
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-26 23:55:47 +0200

    initial commmit

--- philo/src/init.c
+++ philo/src/init.c
@@ -5,46 +5,26 @@
 /*                                                     +:+                    */
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/07/22 20:32:56 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/07/26 13:54:23 by pmolnar       ########   odam.nl         */
+/*   Created: 2022/07/26 20:58:51 by pmolnar       #+#    #+#                 */
+/*   Updated: 2022/07/26 22:50:23 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
-static int	create_philos(t_sim *data)
-{
+void	init_resources(t_sim *data)
+{	
 	uint16_t	i;
 
-	data->philo = malloc(data->attr[N_PHILO] * sizeof(t_philo));
-	if (data->philo == NULL)
-		return (thrw_err(MALLOC_ERR_MSG, __FILE__, __LINE__));
 	i = 0;
 	while (i < data->attr[N_PHILO])
 	{
 		data->philo[i].id = i + 1;
-		data->philo[i].m = &data->mutex[START];
+		data->philo[i].sim_data = data;
 		i++;
 	}
-	return (EXIT_SUCCESS);
-}
-
-static int	create_forks(t_sim *data)
-{
-	data->fork = malloc(data->attr[N_PHILO] * sizeof(t_mutex));
-	if (data->fork == NULL)
-		return (thrw_err(MALLOC_ERR_MSG, __FILE__, __LINE__));
-	return (EXIT_SUCCESS);
-}
-
-int	alloc_sim_resources(t_sim *data)
-{
-	if (create_philos(data))
-		return (EXIT_FAILURE);
-	if (create_forks(data))
-	{
-		free (data->philo);
-		return (EXIT_FAILURE);
-	}
-	return (EXIT_SUCCESS);
+	i = 0;
+	while (i < QUEUE_SIZE)
+		data->queue[i++].status = UNDEFINED;
+	data->is_running = true;
 }
