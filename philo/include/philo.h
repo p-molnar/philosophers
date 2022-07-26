53bfeb9f3a55cb278e512f97f8745ad89d156560
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-26 23:45:56 +0200

    change return types to stdint types

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/07 12:30:53 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/07/26 13:12:07 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/07/26 23:20:02 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,10 @@
 
 # include <philo_data_structures.h>
 
-# ifndef FORMAT_STR
-#  define FORMAT_STR "%zu %zu %s\n"
-# endif 
+# ifndef LOG_FMT
+// #  define LOG_FMT "%-16lu %d %s\n"
+#  define LOG_FMT "%-16d %d %d\n"
+# endif
 
 enum e_arg
 {
@@ -39,7 +40,18 @@ enum e_arg
 
 enum e_mutex
 {
-	START,
+	INIT,
+	LOG,
+	QUEUE,
+};
+
+enum e_status
+{
+	UNDEFINED,
+	THINKING,
+	TAKING_FORK,
+	EATING,
+	SLEEPING,
 };
 
 enum e_util_thread
@@ -47,6 +59,7 @@ enum e_util_thread
 	CHECKER,
 	PRINTER,
 };
+
 # define INPUT_ERR_MSG "invalid input"
 # define MUTEX_ERR_MSG "mutex error"
 # define MALLOC_ERR_MSG "malloc error"
@@ -56,34 +69,42 @@ enum e_util_thread
 <eat duration> <sleep duration> [<min eat count by each philo>]\n"
 
 // input.c
-int		parse_args(int argc, char *argv[], t_sim *data);
+uint16_t	parse_args(int argc, char *argv[], t_sim *data);
 
 // input_util.c
-int		ft_atoi(char *str);
+int32_t		ft_atoi(char *str);
 
 // error_handling.c
-int		thrw_err(char *err_msg, char *err_file, int err_line);
+uint16_t	thrw_err(char *err_msg, char *err_file, int err_line);
 
 // time.c
-t_time	get_time(void);
-long	time_delta_msec(t_time t1, t_time t2);
-void	precise_sleep(long duration);
+t_time		get_time(void);
+uint32_t	time_delta_msec(t_time t1, t_time t2);
+void		precise_sleep(long duration);
+
+// init.c
+void		init_resources(t_sim *data);
 
 // thread.c
-int		create_threads(t_sim *data);
-int		join_threads(t_sim *data);
+uint16_t	create_threads(t_sim *data);
+uint16_t	join_threads(t_sim *data);
 
 // mutex.c
-int		init_mutexes(t_sim *data);
-int		destroy_mutexes(t_sim *data);
+uint16_t	init_mutexes(t_sim *data);
+uint16_t	destroy_mutexes(t_sim *data);
 
 // simulation.c
-int		alloc_sim_resources(t_sim *data);
+uint16_t	alloc_sim_resources(t_sim *data);
 
 // philo.c
-int		simulation(t_philo *philo);
+uint16_t	simulation(t_philo *philo);
 
 // util_threads.c
-int		checker(t_sim *data);
-int		printer(t_sim *data);
+uint16_t	checker(t_sim *data);
+uint16_t	printer(t_sim *data);
+
+// printer.c
+void		log_status(t_philo *data, uint16_t status, t_time time);
+void		print_status(t_log log);
+
 #endif
