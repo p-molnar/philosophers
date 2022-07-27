f2a2c2d1eacc995039c3587fda88b7839b7d8540
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-27 15:30:30 +0200

    add philo status message, change counter in log_status

--- philo/src/log.c
+++ philo/src/log.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   printer.c                                          :+:    :+:            */
+/*   log.c                                              :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/26 20:36:23 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/07/26 23:03:57 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/07/27 11:30:55 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 void	print_status(t_log log)
 {
-	printf(LOG_FMT, log.timestamp, log.philo_id, log.status);
+	const char			*msg[5] = {"is thinking", "has taken a fork", \
+									"is eating", "is sleeping", "died"};
+
+	printf(LOG_FMT, log.timestamp, log.philo_id, msg[log.status - 1]);
 }
 
 void	log_status(t_philo *data, uint16_t status, t_time time)
 {
-	static u_int16_t	loc;
+	static u_int16_t	i;
 	t_log				log;
 
 	pthread_mutex_lock(&data->sim_data->mutex[LOG]);
@@ -27,9 +30,9 @@ void	log_status(t_philo *data, uint16_t status, t_time time)
 	log.status = status;
 	log.philo_id = data->id;
 	pthread_mutex_lock(&data->sim_data->mutex[QUEUE]);
-	data->sim_data->queue[loc] = log;
-	if (++loc == QUEUE_SIZE)
-		loc = 0;
+	data->sim_data->queue[i] = log;
+	if (++i == QUEUE_SIZE)
+		i = 0;
 	pthread_mutex_unlock(&data->sim_data->mutex[QUEUE]);
 	pthread_mutex_unlock(&data->sim_data->mutex[LOG]);
 }
