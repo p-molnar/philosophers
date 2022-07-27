afdabcf9883f64730bf305223d104b759688fb00
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-07-28 00:36:07 +0200

    add checker func

--- philo/src/util_threads.c
+++ philo/src/util_threads.c
@@ -6,33 +6,56 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/25 14:06:36 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/07/26 22:59:41 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/07/28 00:17:56 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
-uint16_t	checker(t_sim *data)
-{
-	(void) data;
-	printf("checker started\n");
-	return (EXIT_SUCCESS);
+void	checker(t_sim *data)
+{	
+	uint16_t	i;
+	t_time		curr_time;
+
+	i = 0;
+	pthread_mutex_lock(&data->mutex[PHILO]);
+	pthread_mutex_unlock(&data->mutex[PHILO]);
+	while (data->is_running)
+	{
+		curr_time = get_time();
+		if (time_delta_msec(data->philo[i].last_ate, curr_time) > (uint32_t) data->attr[T_DIE])
+		{
+			log_status(&data->philo[i], DIED, curr_time);
+			data->is_running = false;
+			return ;
+		}
+		if (++i == data->attr[N_PHILO])
+			i = 0;
+		usleep(500);
+	}
 }
 
-uint16_t	printer(t_sim *data)
+void	printer(t_sim *data)
 {
 	uint16_t	i;
 
 	i = 0;
-	while (data->is_running)
+	while (true)
 	{
+		pthread_mutex_lock(&data->mutex[QUEUE]);
 		if (data->queue[i].status != UNDEFINED)
 		{
 			print_status(data->queue[i]);
+			if (data->queue[i].status == DIED)
+			{
+				unlock_all(data);
+				return ;
+			}
+			data->queue[i].status = UNDEFINED;
 			if (++i == QUEUE_SIZE)
 				i = 0;
 		}
+		pthread_mutex_unlock(&data->mutex[QUEUE]);
 		usleep(500);
 	}
-	return (EXIT_SUCCESS);
 }
