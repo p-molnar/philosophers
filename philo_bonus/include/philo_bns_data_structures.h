659100c3f8219994c7b9beefd9892c3930b23842
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-11 11:34:22 +0200

    clean up irrelevant members for the bonus

--- philo_bonus/include/philo_bns_data_structures.h
+++ philo_bonus/include/philo_bns_data_structures.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   philo_data_structures.h                            :+:    :+:            */
+/*   philo_bns_data_structures.h                        :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/07 12:19:42 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/01 14:33:55 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/11 11:34:05 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_DATA_STRUCTURES_H
-# define PHILO_DATA_STRUCTURES_H
+#ifndef PHILO_BNS_DATA_STRUCTURES_H
+# define PHILO_BNS_DATA_STRUCTURES_H
 
 # define MUTEX_SIZE 5
 # define QUEUE_SIZE 1024
@@ -19,18 +19,14 @@
 # include <stdint.h>
 
 typedef struct timeval	t_time;
-typedef pthread_mutex_t	t_mutex;
 typedef struct s_sim	t_sim;
 
 typedef struct s_philo
 {
-	pthread_t	thread;
 	t_time		last_ate;
 	uint16_t	id;
 	t_sim		*sim_data;
 	uint16_t	eat_count;
-	t_mutex		*fork[2];
-	t_mutex		self;
 
 }	t_philo;
 
@@ -44,10 +40,7 @@ typedef struct s_log
 typedef struct s_sim
 {
 	int16_t		attr[5];
-	t_mutex		mutex[MUTEX_SIZE];
-	t_mutex		*fork;
 	t_philo		*philo;
-	pthread_t	thread[2];
 	t_log		queue[QUEUE_SIZE];
 	bool		is_running;
 	t_time		start_time;
