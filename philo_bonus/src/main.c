fc1a2097f3349931a8050dcd2700a30824caf50d
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-11 22:29:49 +0200

    draw up pseudo code

--- philo_bonus/src/main.c
+++ philo_bonus/src/main.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/07 13:25:32 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/07 13:40:08 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/11 22:25:38 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,15 @@ int32_t	main(int argc, char *argv[])
 	t_sim	data;
 
 	if (parse_args(argc, argv, &data))
-		return (EXIT_SUCCESS);
+		return (EXIT_FAILURE);
+	init_resources(&data);
+	// if (init_processes(&data))
+	// 	return (EXIT_FAILURE);
+	// if (init_aux_threads(&data))
+	// 	return (EXIT_FAILURE);
+	// if (start_simulate(&data))
+	// 	return (EXIT_FAILURE);
+	// if (clean_up(&data))
+	// 	return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
