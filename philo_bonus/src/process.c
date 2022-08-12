1c3d42a94f96231ba2bc4a32028c00ebc8fe1c52
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-13 00:59:40 +0200

    play around with fork

--- philo_bonus/src/process.c
+++ philo_bonus/src/process.c
@@ -6,22 +6,47 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/11 14:01:58 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/11 18:37:13 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/12 15:04:50 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bns.h>
 
-bool	init_processes(t_sim *data)
+void	philo(uint16_t i)
 {
-	uint32_t	id;
+	int	idx = 0;
+
+	while (idx < 5)
+	{
+		printf("%d is running\n", i);
+		sleep(1);
+		idx++;
+	}
+	exit(EXIT_SUCCESS);
+}
+
+bool	start_processes(t_sim *data)
+{
 	uint32_t	i;
+	uint32_t	*pid;
 
 	i = 0;
-	while (i < data->attr[N_PHILO])
+	pid = malloc(data->attr[N_PHILO] * sizeof(uint32_t));
+	if (pid == NULL)
+		thrw_err(MALLOC_ERR_MSG, __FILE__, __LINE__);
+	while (i < (uint32_t)data->attr[N_PHILO])
 	{
-		id = fork();
-		if (id == 0)
+
+		pid[i] = fork();
+		if (pid[i] < 0)
+			thrw_err (PROCESS_ERR_MSG, __FILE__, __LINE__);
+		if (pid[i] == 0)
+		{
+			printf("abc\n");
+			return (EXIT_SUCCESS);
+		}
 		i++;
 	}
+	printf("abc\n");
+	return (EXIT_SUCCESS);
 }
