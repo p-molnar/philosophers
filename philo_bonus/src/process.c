4b527d01da5e8af21472739499b68c71167c6e73
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-14 00:14:28 +0200

    add binary semaphores

--- philo_bonus/src/process.c
+++ philo_bonus/src/process.c
@@ -6,47 +6,46 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/11 14:01:58 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/12 15:04:50 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/14 00:06:47 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bns.h>
 
-void	philo(uint16_t i)
+bool	init_philo(t_philo *philo)
 {
-	int	idx = 0;
-
-	while (idx < 5)
-	{
-		printf("%d is running\n", i);
-		sleep(1);
-		idx++;
-	}
-	exit(EXIT_SUCCESS);
+	sem_wait(philo->sim_data->sem[SEM_START]);
+	sem_post(philo->sim_data->sem[SEM_START]);
+	printf("philo id: %d\n", philo->id);
+	return (EXIT_SUCCESS);
 }
 
 bool	start_processes(t_sim *data)
 {
 	uint32_t	i;
 	uint32_t	*pid;
+	int			wstatus;
 
 	i = 0;
 	pid = malloc(data->attr[N_PHILO] * sizeof(uint32_t));
 	if (pid == NULL)
 		thrw_err(MALLOC_ERR_MSG, __FILE__, __LINE__);
+	sem_wait(data->sem[SEM_START]);
 	while (i < (uint32_t)data->attr[N_PHILO])
 	{
-
 		pid[i] = fork();
-		if (pid[i] < 0)
-			thrw_err (PROCESS_ERR_MSG, __FILE__, __LINE__);
 		if (pid[i] == 0)
 		{
-			printf("abc\n");
-			return (EXIT_SUCCESS);
+			init_philo(&data->philo[i]);
 		}
 		i++;
 	}
-	printf("abc\n");
+	sem_post(data->sem[SEM_START]);
+	i = 0;
+	while (i < (uint32_t)data->attr[N_PHILO])
+	{
+		waitpid(pid[i], &wstatus, WIFEXITED(wstatus));
+		i++;
+	}
 	return (EXIT_SUCCESS);
 }
