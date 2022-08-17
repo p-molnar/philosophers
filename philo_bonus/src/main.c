912b2b8d0f06f246a10530074b7069e312492475
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-17 22:56:58 +0200

    messing around with semaphore

--- philo_bonus/src/main.c
+++ philo_bonus/src/main.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/07 13:25:32 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/12 11:42:03 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/17 22:46:26 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ int32_t	main(int argc, char *argv[])
 	init_resources(&data);
 	if (start_processes(&data))
 		return (EXIT_FAILURE);
+	sem_unlink("/1");
+	sem_close(data.sem[SEM_START]);
 	// if (start_aux_threads(&data))
 	// 	return (EXIT_FAILURE);
 	// if (start_simulate(&data))
