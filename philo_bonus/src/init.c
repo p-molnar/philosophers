62cbdf3c910dbfe402448da853065258c16c9403
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-20 19:19:05 +0200

    single out open_semaphores to main

--- philo_bonus/src/init.c
+++ philo_bonus/src/init.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/26 20:58:51 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/19 22:47:40 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/20 17:15:39 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,6 @@ static void	init_queue(t_sim *data)
 void	init_resources(t_sim *data)
 {
 	init_philos(data);
-	open_semaphores(data);
 	init_queue(data);
 	data->is_running = true;
 }
