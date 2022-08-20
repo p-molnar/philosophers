4651653ec9e797591bf6392870981ad73236c01a
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-20 13:28:33 +0200

    add routine skeleton

--- philo_bonus/src/philo.c
+++ philo_bonus/src/philo.c
@@ -6,16 +6,47 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/19 16:51:36 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/19 22:35:39 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/20 13:19:41 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bns.h>
 
+static void	philo_think(t_philo *philo)
+{
+	// log_status(philo, THINKING, get_time());
+	(void) philo;
+}
+
+static void	philo_eat(t_philo *philo)
+{
+	(void) philo;
+}
+
+static void	philo_sleep(t_philo *philo)
+{
+	(void) philo;
+}
+
 bool	simulate(t_philo *philo)
 {
+	uint16_t	status;
+
 	sem_wait(philo->sim_data->sem[START_LOCK]);
 	sem_post(philo->sim_data->sem[START_LOCK]);
-	printf("id: %d\n", philo->id);
+	philo_think(philo);
+	if (philo->id % 2)
+		precise_sleep(1000);
+	status = EATING;
+	while (1)
+	{
+		if (status == THINKING)
+			philo_think(philo);
+		else if (status == EATING)
+			philo_eat(philo);
+		else if (status == SLEEPING)
+			philo_sleep(philo);
+		status = (status + 1) % STATUS__COUNT;
+	}
 	return (EXIT_SUCCESS);
 }
