c8081a937a07dd3174b1efc8b7c6091fda9cb780
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-24 22:25:20 +0200

    add thread init to each child process

--- philo_bonus/src/process.c
+++ philo_bonus/src/process.c
@@ -6,33 +6,40 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/11 14:01:58 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/20 13:06:16 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/24 18:53:45 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bns.h>
+#include <errno.h>
+#include <string.h>
 
 bool	start_philo_processes(t_sim *data)
 {
 	uint16_t	i;
+	int32_t		pid;
 
-	i = 0;
 	data->philo_pid = malloc(data->attr[N_PHILO] * sizeof(uint32_t));
 	if (data->philo_pid == NULL)
 		thrw_err(MALLOC_ERR_MSG, __FILE__, __LINE__);
 	sem_wait(data->sem[START_LOCK]);
-	while (i < (uint32_t)data->attr[N_PHILO])
+	i = 0;
+	while (i < data->attr[N_PHILO])
 	{
-		data->philo_pid[i] = fork();
-		if (data->philo_pid[i] == 0)
+		pid = fork();
+		if (pid == -1)
+			return (thrw_err(PROCESS_ERR_MSG, __FILE__, __LINE__));
+		else if (pid == 0)
 		{
-			simulate(&data->philo[i]);
+			start_aux_threads(data);
+			simulate(&(data->philo[i]));
+			join_aux_threads(data);
 			exit(EXIT_SUCCESS);
 		}
+		data->philo_pid[i] = pid;
 		i++;
 	}
-	data->start_time = get_time();
-	sem_post(data->sem[START_LOCK]);
+	sem_post(data->sem[START_LOCK]);	
 	return (EXIT_SUCCESS);
 }
 
@@ -44,7 +51,10 @@ bool	wait_philo_processes(t_sim *data)
 	while (i < data->attr[N_PHILO])
 	{
 		if (waitpid(data->philo_pid[i], NULL, 0) == -1)
+		{
+			printf("error: %s\n", strerror(errno));
 			thrw_err(PROCESS_ERR_MSG, __FILE__, __LINE__);
+		}
 		i++;
 	}
 	return (EXIT_SUCCESS);
