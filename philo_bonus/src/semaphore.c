162389ded037576504e4bdbeccb664a97f7704cc
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-25 17:18:44 +0200

    unlink all sems first and then open

--- philo_bonus/src/semaphore.c
+++ philo_bonus/src/semaphore.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/19 21:37:46 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/20 18:53:09 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/25 01:01:48 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,13 @@ bool	open_semaphores(t_sim *data)
 	uint16_t	n_philo;
 
 	n_philo = data->attr[N_PHILO];
-	data->sem[START_LOCK] = sem_open("/sem_start", IPC_CREAT, 0660, 1);
-	data->sem[PRINTER_LOCK] = sem_open("/sem_print", IPC_CREAT, 0660, 1);
-	data->sem[FORK] = sem_open("/sem_fork", IPC_CREAT, 0660, n_philo);
-	data->sem[LOG_RW] = sem_open("/sem_log_rw", IPC_CREAT, 0660, 5);
-	data->sem[QUEUE_RW] = sem_open("/sem_queue_rw", IPC_CREAT, 0660, 1);
+	unlink_semaphores(data);
+	data->sem[START_LOCK] = sem_open("/sem_start", IPC_CREAT | IPC_EXCL, 0660, 1);
+	data->sem[CHECKER_LOCK] = sem_open("/sem_checker", IPC_CREAT | IPC_EXCL, 0660, 1);
+	data->sem[PRINTER_LOCK] = sem_open("/sem_print", IPC_CREAT | IPC_EXCL, 0660, 1);
+	data->sem[FORK] = sem_open("/sem_fork", IPC_CREAT | IPC_EXCL, 0660, n_philo);
+	data->sem[LOG_RW] = sem_open("/sem_log_rw", IPC_CREAT | IPC_EXCL, 0660, 1);
+	data->sem[QUEUE_RW] = sem_open("/sem_queue_rw", IPC_CREAT | IPC_EXCL, 0660, 1);
 	if (data->sem[START_LOCK] == SEM_FAILED
 		|| data->sem[PRINTER_LOCK] == SEM_FAILED
 		|| data->sem[FORK] == SEM_FAILED
@@ -35,18 +37,21 @@ bool	open_semaphores(t_sim *data)
 	return (EXIT_SUCCESS);
 }
 
+bool	unlink_semaphores(t_sim *data)
+{
+	(void) data;
+	sem_unlink("/sem_start");
+	sem_unlink("/sem_checker");
+	sem_unlink("/sem_fork");
+	sem_unlink("/sem_print");
+	sem_unlink("/sem_log_rw");
+	sem_unlink("/sem_queue_rw");
+	return (EXIT_SUCCESS);
+}
+
 bool	close_semaphores(t_sim *data)
 {
-	if (sem_unlink("/sem_start") == -1)
-		return (thrw_err(SEM_ERR_MSG, __FILE__, __LINE__));
-	if (sem_unlink("/sem_fork") == -1)
-		return (thrw_err(SEM_ERR_MSG, __FILE__, __LINE__));
-	if (sem_unlink("/sem_print") == -1)
-		return (thrw_err(SEM_ERR_MSG, __FILE__, __LINE__));
-	if (sem_unlink("/sem_log_rw") == -1)
-		return (thrw_err(SEM_ERR_MSG, __FILE__, __LINE__));
-	if (sem_unlink("/sem_queue_rw") == -1)
-		return (thrw_err(SEM_ERR_MSG, __FILE__, __LINE__));
+	unlink_semaphores(data);
 	if (sem_close(data->sem[START_LOCK]) == -1)
 		return (thrw_err(PROCESS_ERR_MSG, __FILE__, __LINE__));
 	if (sem_close(data->sem[PRINTER_LOCK]) == -1)
