eb0a30eb2b31b7a726bcd3f1c0b3335a092636aa
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-25 17:17:46 +0200

    remove redundant parts

--- philo_bonus/src/printer.c
+++ philo_bonus/src/printer.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/25 14:06:36 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/24 22:22:29 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/25 17:04:42 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,12 @@
 #include <errno.h>
 #include <string.h>
 
-static void	print_status(t_log log, t_sim *data)
+static void	print_status(t_log log)
 {
 	const char			*msg[6] = {"is thinking", "is eating", "is sleeping", \
 									"has taken a fork", "died", "all fed"};
-	uint16_t			i;
 
-	if (log.status == ALL_FED)
-	{
-		printf("\n%-d all philosophers have eaten\n", log.timestamp);
-		printf("\n-----   SUMMARY   -----\n");
-		i = 0;
-		while (i < data->attr[N_PHILO])
-		{
-			// pthread_mutex_lock(&data->philo[i].self);
-			printf("philo %d %s: %d time(s)\n", data->philo[i].id, "ate",
-				data->philo[i].eat_count);
-			// pthread_mutex_unlock(&data->philo[i].self);
-			i++;
-		}
-		printf("-----------------------\n");
-	}
-	else
-		printf(LOG_FMT, log.timestamp, log.philo_id, msg[log.status]);
+	printf(LOG_FMT, log.timestamp, log.philo_id, msg[log.status]);
 }
 
 void	log_status(t_philo *data, uint16_t status, t_time time)
@@ -44,22 +27,15 @@ void	log_status(t_philo *data, uint16_t status, t_time time)
 	static u_int16_t	i;
 	t_log				log;
 
-	// if(sem_wait(data->sim_data->sem[LOG_RW]) == -1)
-	// {
-	// 	printf("error printer: %s\n", strerror(errno));
-	// }
-	// printf("somethings happening: %d\n", __LINE__);
+	sem_wait(data->sim_data->sem[LOG_RW]);
 	log.timestamp = time_delta_msec(data->sim_data->start_time, time);
 	log.status = status;
 	log.philo_id = data->id;
-	// sem_wait(data->sim_data->sem[QUEUE_RW]);
+	sem_wait(data->sim_data->sem[QUEUE_RW]);
 	data->sim_data->queue[i] = log;
-	// printf("process: %ld, %d\n", data->sim_data->start_time.tv_sec, data->sim_data->start_time.tv_usec);
-	// printf("philo %d, new = %d\n", data->id, *data->sim_data->new);
-	// printf("%d, %d, %c\n", log.timestamp, log.philo_id, "TES"[log.status]);
 	i = (i + 1) % QUEUE__SIZE;
-	// sem_post(data->sim_data->sem[QUEUE_RW]);
-	// sem_post(data->sim_data->sem[LOG_RW]);
+	sem_post(data->sim_data->sem[QUEUE_RW]);
+	sem_post(data->sim_data->sem[LOG_RW]);
 }
 
 void	*printer_thread(void *arg)
@@ -69,22 +45,23 @@ void	*printer_thread(void *arg)
 
 	data = arg;
 	i = 0;
-	while (true)
+	sem_wait(data->sem[PRINTER_LOCK]);
+	sem_post(data->sem[PRINTER_LOCK]);
+	while (1)
 	{
-		// sem_wait(data->sem[QUEUE_RW]);
+		sem_wait(data->sem[QUEUE_RW]);
 		if (data->queue[i].status != UNDEFINED)
 		{
-			print_status(data->queue[i], data);
+			print_status(data->queue[i]);
 			if (data->queue[i].status == DIED
 				|| data->queue[i].status == ALL_FED)
 			{
-				// unlock_all(data);
 				return (NULL);
 			}
 			data->queue[i].status = UNDEFINED;
 			i = (i + 1) % QUEUE__SIZE;
 		}
-		// sem_post(data->sem[QUEUE_RW]);
+		sem_post(data->sem[QUEUE_RW]);
 		usleep(500);
 	}
 	return (NULL);
@@ -92,20 +69,16 @@ void	*printer_thread(void *arg)
 
 bool	start_aux_threads(t_sim *data)
 {
-	// sem_wait(data->sem[PRINTER_LOCK]);
 	pthread_create(&data->thread[PRINTER], NULL, &printer_thread, (void *)data);
 	pthread_create(&data->thread[CHECKER], NULL, &checker_thread, (void *)data);
-	// sem_post(data->sem[PRINTER_LOCK]);
 	return (EXIT_SUCCESS);
 }
 
 bool	join_aux_threads(t_sim *data)
 {
-	// sem_wait(data->sem[PRINTER_LOCK]);
 	pthread_join(data->thread[PRINTER], NULL);
 	pthread_detach(data->thread[PRINTER]);
 	pthread_join(data->thread[CHECKER], NULL);
 	pthread_detach(data->thread[CHECKER]);
-	// sem_post(data->sem[PRINTER_LOCK]);
 	return (EXIT_SUCCESS);
 }
