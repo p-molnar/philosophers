5d8f1ca7a225670aabb1a027fa1f178a7463fb04
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-25 17:16:32 +0200

    move init under each process

--- philo_bonus/src/init.c
+++ philo_bonus/src/init.c
@@ -6,24 +6,19 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/26 20:58:51 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/24 12:50:20 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/25 15:40:06 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bns.h>
 
-static void	init_philos(t_sim *data)
+void	init_philo(t_sim *data, uint16_t id)
 {
-	uint16_t	i;
-
-	i = 0;
-	while (i < data->attr[N_PHILO])
-	{
-		data->philo[i].id = i + 1;
-		data->philo[i].eat_count = 0;
-		data->philo[i].sim_data = data;
-		i++;
-	}
+		data->philo.id = id;
+		data->philo.forks_in_hand= 0;
+		data->philo.eat_count = 0;
+		data->philo.sim_data = data;
+		data->philo.sim_data->sim_running = true;
 }
 
 static void	init_queue(t_sim *data)
@@ -37,7 +32,5 @@ static void	init_queue(t_sim *data)
 
 void	init_resources(t_sim *data)
 {
-	init_philos(data);
 	init_queue(data);
-	data->is_running = true;
 }
