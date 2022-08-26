e61ef8a3ea6f0bf65d0309a1277f3fa3bc1f4844
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-26 17:20:36 +0200

    clean up code

--- philo_bonus/src/init.c
+++ philo_bonus/src/init.c
@@ -6,31 +6,29 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/26 20:58:51 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/25 15:40:06 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/25 20:29:32 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bns.h>
 
-void	init_philo(t_sim *data, uint16_t id)
-{
-		data->philo.id = id;
-		data->philo.forks_in_hand= 0;
-		data->philo.eat_count = 0;
-		data->philo.sim_data = data;
-		data->philo.sim_data->sim_running = true;
-}
-
-static void	init_queue(t_sim *data)
+void	init_queue(t_sim *data)
 {
 	uint16_t	i;
 
 	i = 0;
 	while (i < QUEUE__SIZE)
-		data->queue[i++].status = UNDEFINED;
+	{
+		data->queue[i].status = UNDEFINED;
+		i++;
+	}
 }
 
-void	init_resources(t_sim *data)
+void	init_philo(t_sim *data, uint16_t id)
 {
-	init_queue(data);
+		data->philo.id = id;
+		data->philo.forks_in_hand = 0;
+		data->philo.eat_count = 0;
+		data->philo.sim_data = data;
+		data->philo.sim_data->sim_running = true;
 }
