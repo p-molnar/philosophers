e19cf607bb968db6d66f795e6b6a74a0c7424cf1
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-08-26 17:22:21 +0200

    add exit code checker to main process

--- philo_bonus/src/process.c
+++ philo_bonus/src/process.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/11 14:01:58 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/25 20:26:21 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/26 16:03:03 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,14 +38,14 @@ bool	create_child_processes(t_sim *data)
 			start_aux_threads(data);
 			simulate(&data->philo);
 			join_aux_threads(data);
-			exit(data->philo.status);
+			exit(EXIT_SUCCESS);
 		}
 		data->philo_pid[i] = pid;
 		i++;
 	}
 	sem_post(data->sem[START_LOCK]);
 	sem_post(data->sem[PRINTER_LOCK]);
-	precise_usleep(data->attr[N_PHILO]);
+	precise_msleep(1);
 	sem_post(data->sem[CHECKER_LOCK]);
 	return (EXIT_SUCCESS);
 }
@@ -62,28 +62,35 @@ void	kill_all_child_process(t_sim *data, uint16_t except)
 		{
 			if (kill(data->philo_pid[i], SIGKILL) == -1)
 				printf("kill error: %s\n", strerror(errno));
-			printf("kill: %d\n", data->philo_pid[i]);
+			// printf("kill: %d\n", data->philo_pid[i]);
 		}
 		i++;
 	}
 }
 
-bool	wait_philo_processes(t_sim *data)
+bool	wait_child_processes(t_sim *data)
 {	
 	uint16_t	i;
+	int16_t		pid;
 	int			status;
+	int			fed_count;
 
+	fed_count = 0;
 	i = 0;
 	while (1)
 	{
 		// printf("checking: %d\n", data->philo_pid[i]);
-		if (waitpid(data->philo_pid[i], &status, WNOHANG) == -1)
+		pid = waitpid(data->philo_pid[i], &status, WNOHANG);
+		if (pid == -1)
 			return (thrw_err(PROCESS_ERR_MSG, __FILE__, __LINE__));
-		else if (WEXITSTATUS(status) == DIED)
+		else if (WEXITSTATUS(status) == FED)
+			fed_count++;
+		if (WEXITSTATUS(status) == DIED || fed_count == data->attr[N_PHILO])
 		{
 			kill_all_child_process(data, i);
 			return (EXIT_SUCCESS);
 		}
+		usleep(500);
 		i = (i + 1) % data->attr[N_PHILO];
 	}
 	return (EXIT_SUCCESS);
