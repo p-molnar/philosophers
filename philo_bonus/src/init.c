de122d3fdbca987ad46a5f6a496c21d9ef522227
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-04 23:04:42 +0200

    change initalisation

--- philo_bonus/src/init.c
+++ philo_bonus/src/init.c
@@ -6,13 +6,13 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/26 20:58:51 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/25 20:29:32 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/09/04 23:00:15 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bns.h>
 
-void	init_queue(t_sim *data)
+void	init_resources(t_sim *data)
 {
 	uint16_t	i;
 
@@ -22,13 +22,22 @@ void	init_queue(t_sim *data)
 		data->queue[i].status = UNDEFINED;
 		i++;
 	}
+	i = 0;
+	while (i < data->attr[N_PHILO])
+		data->philo_exited[i++] = false;
 }
 
-void	init_philo(t_sim *data, uint16_t id)
+sem_t	*gen_philo_sem(t_sim *data)
 {
-		data->philo.id = id;
+	
+}
+
+void	init_philo(t_sim *data, uint16_t i)
+{
+		data->philo.id = i + 1;
 		data->philo.forks_in_hand = 0;
 		data->philo.eat_count = 0;
 		data->philo.sim_data = data;
 		data->philo.sim_data->sim_running = true;
+		data->philo.self = gen_philo_sem(data);
 }
