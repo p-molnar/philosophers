c4c552dda11e754f96cb8c651bf6498df1971aca
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-04 23:05:30 +0200

    change member name

--- philo_bonus/src/alloc.c
+++ philo_bonus/src/alloc.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/29 23:56:12 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/29 23:59:23 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/08/30 16:20:28 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 bool	alloc_resources(t_sim *data)
 {	
-	data->philo_pid = malloc(data->attr[N_PHILO] * sizeof(uint32_t));
-	if (data->philo_pid == NULL)
+	data->pid_arr = malloc(data->attr[N_PHILO] * sizeof(uint32_t));
+	if (data->pid_arr == NULL)
 		thrw_err(MALLOC_ERR_MSG, __FILE__, __LINE__);
 	data->philo_exited = malloc(data->attr[N_PHILO] * sizeof(bool));
 	if (data->philo_exited == NULL)
