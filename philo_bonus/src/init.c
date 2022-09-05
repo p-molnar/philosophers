12ed2514ac5e67b6b40ce4dbad7d8af09f3ea4ba
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-05 10:28:18 +0200

    add semaphore generator for individual philos

--- philo_bonus/src/init.c
+++ philo_bonus/src/init.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/26 20:58:51 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/09/04 23:00:15 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/09/05 10:24:31 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,39 @@ void	init_resources(t_sim *data)
 		data->philo_exited[i++] = false;
 }
 
-sem_t	*gen_philo_sem(t_sim *data)
+void	gen_philo_sem(t_sim *data)
 {
-	
+	char		name[32];
+	char		*id;
+	uint16_t	i;
+
+	id = ft_itoa(data->philo.id);
+	i = 0;
+	while (i < 7)
+	{
+		name[i] = "/philo_"[i];
+		i++;
+	}
+	i = 0;
+	while (i < ft_strlen(id))
+	{
+		name[i + 7] = id[i];
+		i++;
+	}
+	name[i + 7] = '\0';
+	free (id);
+	data->philo.self = sem_open(name, O_CREAT | O_EXCL, 000664, 1);
+	if (data->philo.self == SEM_FAILED)
+		thrw_err(SEM_ERR_MSG, __FILE__, __LINE__);
+	sem_unlink(name);
 }
 
 void	init_philo(t_sim *data, uint16_t i)
 {
-		data->philo.id = i + 1;
-		data->philo.forks_in_hand = 0;
-		data->philo.eat_count = 0;
-		data->philo.sim_data = data;
-		data->philo.sim_data->sim_running = true;
-		data->philo.self = gen_philo_sem(data);
+	data->philo.id = i + 1;
+	data->philo.forks_in_hand = 0;
+	data->philo.eat_count = 0;
+	data->philo.sim_data = data;
+	data->philo.sim_data->sim_running = true;
+	gen_philo_sem(data);
 }
