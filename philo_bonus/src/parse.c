12d26a94b498b270e1f3528c32bc2231c1f377dc
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-06 10:26:59 +0200

    refactor code, collect string fns into one src file, rename file

--- philo_bonus/src/parse.c
+++ philo_bonus/src/parse.c
@@ -6,43 +6,12 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/07 12:54:06 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/08/07 13:34:07 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/09/06 10:24:32 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bns.h>
 
-static uint16_t	ft_isspace(int c)
-{
-	return (c == ' '
-		|| c == '\t'
-		|| c == '\n'
-		|| c == '\v'
-		|| c == '\f'
-		|| c == '\r');
-}
-
-static uint16_t	ft_isdigit(int c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-static int32_t	ft_atoi(char *str)
-{
-	int			num;
-	int			sign;
-	const int	coeff[2] = {+1, -1};
-
-	num = 0;
-	sign = 1;
-	while (*str && ft_isspace(*str))
-		str++;
-	if (*str && (*str == '-' || *str == '+'))
-		sign = coeff[(*str++ == '-')];
-	while (*str && ft_isdigit(*str))
-		num = 10 * num + (*str++ - '0');
-	return (sign * num);
-}
 
 uint16_t	validate_arg(int argc, t_sim *data)
 {
---
+++ philo_bonus/src/string_util.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   init_util.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/09/05 10:24:45 by pmolnar       #+#    #+#                 */
+/*   Updated: 2022/09/06 10:24:48 by pmolnar       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <philo_bns.h>
+
+uint16_t	ft_isspace(int c)
+{
+	return (c == ' '
+		|| c == '\t'
+		|| c == '\n'
+		|| c == '\v'
+		|| c == '\f'
+		|| c == '\r');
+}
+
+uint16_t	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+int32_t	ft_atoi(char *str)
+{
+	int			num;
+	int			sign;
+	const int	coeff[2] = {+1, -1};
+
+	num = 0;
+	sign = 1;
+	while (*str && ft_isspace(*str))
+		str++;
+	if (*str && (*str == '-' || *str == '+'))
+		sign = coeff[(*str++ == '-')];
+	while (*str && ft_isdigit(*str))
+		num = 10 * num + (*str++ - '0');
+	return (sign * num);
+}
+
+uint32_t	ft_strlen(char *s)
+{
+	uint32_t	i;
+
+	i = 0;
+	while (s && s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_itoa(int16_t num)
+{
+	char		*s;
+	uint16_t	i;
+
+	s = malloc(sizeof(char) * 4);
+	if (s == NULL)
+		thrw_err(MALLOC_ERR_MSG, __FILE__, __LINE__);
+	i = 0;
+	while (i < 4)
+	{
+		s[i] = num % 10 + '0';
+		num /= 10;
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
