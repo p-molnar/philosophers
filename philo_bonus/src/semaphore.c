37aad010f3cf0bc3021e348926b469f4339a6f1d
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-15 14:01:37 +0200

    remove unnecessary error check

--- philo_bonus/src/semaphore.c
+++ philo_bonus/src/semaphore.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/19 21:37:46 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/09/05 10:37:11 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/09/15 13:37:43 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,24 @@ bool	semaphore_op(t_sim *data, uint8_t mode)
 	uint16_t			i;
 	const int16_t		oflag = IPC_CREAT | IPC_EXCL;
 	const t_sem_attr	ht[SEM__COUNT] = {[START_LOCK] = {"/sem_start", 1}, \
-						[PRINTER_LOCK] = {"/sem_print", 1}, \
-						[CHECKER_LOCK] = {"/sem_checker", 1}, \
-						[FORK] = {"/sem_fork", data->attr[N_PHILO]}};
+								[PRINTER_LOCK] = {"/sem_print", 1}, \
+								[CHECKER_LOCK] = {"/sem_checker", 1}, \
+								[FORK] = {"/sem_fork", data->attr[N_PHILO]}};
 
 	i = 0;
 	while (i < SEM__COUNT)
 	{
 		if (mode == OPEN)
 		{
-			data->sem[i] = sem_open(ht[i].name, oflag, 0660, ht[i].init_val);
-			if (data->sem[i] == SEM_FAILED)
+			data->generic_sem[i] = sem_open(ht[i].name, \
+				oflag, 0660, ht[i].init_val);
+			if (data->generic_sem[i] == SEM_FAILED)
 				return (thrw_err(SEM_ERR_MSG, __FILE__, __LINE__));
 		}
 		else if (mode == CLOSE)
-		{
-			if (sem_close(data->sem[i]) == -1)
-				return (thrw_err(SEM_ERR_MSG, __FILE__, __LINE__));
-		}
+			sem_close(data->generic_sem[i]);
 		else if (mode == UNLINK)
-		{
 			sem_unlink(ht[i].name);
-				// return (thrw_err(SEM_ERR_MSG, __FILE__, __LINE__));
-		}
 		i++;
 	}
 	return (EXIT_SUCCESS);
