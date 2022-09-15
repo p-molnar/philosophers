18e2116b1f4ecaf152e6f8091aa2f15360c16362
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-15 14:00:55 +0200

    change var name

--- philo_bonus/src/printer.c
+++ philo_bonus/src/printer.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/25 14:06:36 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/09/06 10:21:11by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/09/15 13:34:34 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,12 +31,10 @@ void	log_status(t_philo *data, uint16_t status, t_time time)
 	static u_int16_t	i;
 	t_log				log;
 
-	sem_wait(data->self);
 	log.timestamp = time_delta_msec(data->sim_data->start_time, time);
 	log.status = status;
 	log.philo_id = data->id;
 	data->sim_data->queue[i] = log;
-	sem_post(data->self);
 	i = (i + 1) % QUEUE__SIZE;
 }
 
@@ -48,19 +46,19 @@ void	*child_status_printer(void *arg)
 
 	data = arg;
 	i = 0;
-	sem_wait(data->sem[PRINTER_LOCK]);
-	sem_post(data->sem[PRINTER_LOCK]);
+	sem_wait(data->generic_sem[PRINTER_LOCK]);
+	sem_post(data->generic_sem[PRINTER_LOCK]);
 	while (1)
 	{
-		sem_wait(data->philo.self);
+		sem_wait(data->philo.self_lock);
 		status = data->queue[i].status;
 		if (status != UNDEFINED)
 		{
-			print_status(data->queue[i], data->sem[PRINTER]);
+			print_status(data->queue[i], data->generic_sem[PRINTER]);
 			data->queue[i].status = UNDEFINED;
 			i = (i + 1) % QUEUE__SIZE;
 		}
-		sem_post(data->philo.self);
+		sem_post(data->philo.self_lock);
 		usleep(200 - data->attr[N_PHILO]);
 	}
 	return (NULL);
