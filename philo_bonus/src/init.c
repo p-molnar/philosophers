8459a9257f92c7d3378d1e43b3f4a7dfe32e092c
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-22 11:05:56 +0200

    move semaphore functions

--- philo_bonus/src/init.c
+++ philo_bonus/src/init.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/26 20:58:51 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/09/22 09:28:25 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/09/22 10:18:41 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,50 +22,6 @@ void	init_resources(t_sim *data)
 		data->philo_sem[i] = gen_philo_sem(i);
 		i++;
 	}
-}
-
-char	*gen_sem_name(char *id)
-{
-	char		*name;
-	uint16_t	i;
-
-	name = malloc (32 * sizeof(char));
-	if (name == NULL)
-		handle_err(MALLOC_ERR_MSG, __FILE__, __LINE__, NULL);
-	i = 0;
-	while (i < 7)
-	{
-		name[i] = "/philo_"[i];
-		i++;
-	}
-	i = 0;
-	while (i < ft_strlen(id))
-	{
-		name[i + 7] = id[i];
-		i++;
-	}
-	name[i + 7] = '\0';
-	return (name);
-}
-
-sem_t	*gen_philo_sem(uint16_t philo_idx)
-{
-	char		*id;
-	char		*name;
-	uint16_t	i;
-	sem_t		*sem;
-
-	id = ft_itoa(philo_idx);
-	if (id == NULL)
-		handle_err(MALLOC_ERR_MSG, __FILE__, __LINE__, NULL);
-	i = 0;
-	name = gen_sem_name(id);
-	sem = sem_open(name, O_CREAT | O_EXCL, 000664, 1);
-	if (sem == SEM_FAILED)
-		handle_err(SEM_ERR_MSG, __FILE__, __LINE__, NULL);
-	sem_unlink(name);
-	free (id);
-	return (sem);
 }
 
 void	init_philo(t_sim *data, uint16_t i)
--- philo_bonus/src/semaphore.c
+++ philo_bonus/src/semaphore.c
@@ -6,19 +6,61 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/19 21:37:46 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/09/22 09:26:40 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/09/22 10:51:27 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo_bns.h>
-#include <errno.h>
-#include <string.h>
+
+char	*gen_sem_name(char *id)
+{
+	char		*name;
+	uint16_t	i;
+
+	name = malloc (32 * sizeof(char));
+	if (name == NULL)
+		handle_err(MALLOC_ERR_MSG, __FILE__, __LINE__, NULL);
+	i = 0;
+	while (i < 7)
+	{
+		name[i] = "/philo_"[i];
+		i++;
+	}
+	i = 0;
+	while (i < ft_strlen(id))
+	{
+		name[i + 7] = id[i];
+		i++;
+	}
+	name[i + 7] = '\0';
+	return (name);
+}
+
+sem_t	*gen_philo_sem(uint16_t philo_idx)
+{
+	char		*id;
+	char		*name;
+	uint16_t	i;
+	sem_t		*sem;
+
+	id = ft_itoa(philo_idx);
+	if (id == NULL)
+		handle_err(MALLOC_ERR_MSG, __FILE__, __LINE__, NULL);
+	i = 0;
+	name = gen_sem_name(id);
+	sem = sem_open(name, O_CREAT | O_EXCL, 000664, 1);
+	if (sem == SEM_FAILED)
+		handle_err(SEM_ERR_MSG, __FILE__, __LINE__, NULL);
+	sem_unlink(name);
+	free (id);
+	return (sem);
+}
 
 bool	semaphore_op(t_sim *data, uint8_t mode)
 {
-	uint16_t			i;
-	const int16_t		oflag = IPC_CREAT | IPC_EXCL;
-	const t_sem_attr	ht[SEM__COUNT] = {[START_LOCK] = {"/sem_start", 0}, \
+	uint16_t		i;
+	const int16_t	oflag = O_CREAT | O_EXCL;
+	const t_sem_ht	ht[SEM__COUNT] = {[START_LOCK] = {"/sem_start", 0}, \
 								[PRINTER_LOCK] = {"/sem_print", 0}, \
 								[CHECKER_LOCK] = {"/sem_checker", 0}, \
 								[FORK] = {"/sem_fork", data->attr[N_PHILO]}};
@@ -40,4 +82,12 @@ bool	semaphore_op(t_sim *data, uint8_t mode)
 		i++;
 	}
 	return (EXIT_SUCCESS);
+}
+
+void	unlock_gen_semaphores(t_sim *data)
+{
+	sem_post(data->generic_sem[START_LOCK]);
+	sem_post(data->generic_sem[PRINTER_LOCK]);
+	precise_msleep(1);
+	sem_post(data->generic_sem[CHECKER_LOCK]);
 }
