89bc458333f591f9bd62b3be7ab04b2df193e0af
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-23 09:08:09 +0200

    remove data freeing

--- philo_bonus/src/alloc.c
+++ philo_bonus/src/alloc.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/29 23:56:12 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/09/22 09:22:54 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/09/23 09:00:30 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ bool	alloc_resources(t_sim *data)
 {	
 	data->checker_thread = malloc(data->attr[N_PHILO] * sizeof(pthread_t));
 	if (data->checker_thread == NULL)
-		handle_err(MALLOC_ERR_MSG, __FILE__, __LINE__, data);
+		handle_err(MALLOC_ERR_MSG, __FILE__, __LINE__, NULL);
 	data->child_pid_arr = malloc(data->attr[N_PHILO] * sizeof(pid_t));
 	if (data->child_pid_arr == NULL)
-		handle_err(MALLOC_ERR_MSG, __FILE__, __LINE__, data);
+		handle_err(MALLOC_ERR_MSG, __FILE__, __LINE__, NULL);
 	data->philo_sem = malloc(data->attr[N_PHILO] * sizeof(sem_t *));
 	if (data->philo_sem == NULL)
-		handle_err(MALLOC_ERR_MSG, __FILE__, __LINE__, data);
+		handle_err(MALLOC_ERR_MSG, __FILE__, __LINE__, NULL);
 	return (EXIT_SUCCESS);
 }
