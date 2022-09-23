c4e24d812299c21b6233a5aaaad9a0e8fbc89fe0
Peter Molnar <54889368+p-molnar@users.noreply.github.com>
2022-09-23 09:10:13 +0200

    remove ptr null-check

--- philo_bonus/src/clean_up.c
+++ philo_bonus/src/clean_up.c
@@ -6,7 +6,7 @@
 /*   By: pmolnar <pmolnar@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/22 09:39:06 by pmolnar       #+#    #+#                 */
-/*   Updated: 2022/09/22 09:40:41 by pmolnar       ########   odam.nl         */
+/*   Updated: 2022/09/23 09:09:45 by pmolnar       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 
 void	clean_up(t_sim *data)
 {
-	if (data->checker_thread)
-		free(data->checker_thread);
-	if (data->child_pid_arr)
-		free(data->child_pid_arr);
-	if (data->philo_sem)
-		free(data->philo_sem);
+	free(data->checker_thread);
+	free(data->child_pid_arr);
+	free(data->philo_sem);
 }
